bed8b33b625b1cbb9a3d896ccfdc530a6c0cd37d
Ori Baram <obrm770@gmail.com>
2023-05-10 11:51:52 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,25 @@ import React from "react";
 
 import weatherData from './data/weatherData';
 
-import { WeatherCard } from './components';
+import { WeatherCard, WeatherCardContent } from './components';
 
-import "./styles/App.scss";
+import "./styles/global.scss";
 
 const App: React.FC = () => {
   return (
     <div className='app-container'>
       <h1>Weather Widget</h1>
       <div className='weather-cards-container'>
-        {weatherData.map((cityWeather) => (
-          <WeatherCard key={cityWeather.city} cityWeather={cityWeather} />
+        {weatherData.map(({ city, weather, temperature }) => (
+          <WeatherCard
+            key={city}
+            weather={weather}>
+            <WeatherCardContent
+              city={city}
+              weather={weather}
+              temperature={temperature}
+            />
+          </WeatherCard>
         ))}
       </div>
     </div>
